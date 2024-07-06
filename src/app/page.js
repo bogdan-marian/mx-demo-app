c122273efdf7e9f13ba9d9f21ebfa41aa5f76f1f
bogdan <bogdan.oloeriu@gmail.com>
2024-07-06 11:40:08 +0200

    added jumbotron

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <>
+    <h1>Hello, Next.js!</h1>
+    <Link href="/test-folder">test-folder</Link>
+  </>
 }
