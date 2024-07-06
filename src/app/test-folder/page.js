7dbc07149053cb651a0298a950f6bbf5427cd14f
bogdan <bogdan.oloeriu@gmail.com>
2024-07-06 11:52:22 +0200

    className instead of class

--- src/app/test-folder/page.js
+++ src/app/test-folder/page.js
@@ -1,9 +1,9 @@
 export default function TestFolder() {
-  return <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">test-folder</h1>
-      <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">This is a test folder</p>
-      <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+  return <section className="bg-white dark:bg-gray-900">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">test-folder</h1>
+      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">This is a test folder</p>
+      <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
         <p>Hello test folder</p>
       </div>
     </div>
