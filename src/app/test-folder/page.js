ec29709f33f76db88db2e80796c21545c5afb916
bogdan <bogdan.oloeriu@gmail.com>
2024-07-06 12:56:30 +0200

    added network provider

--- src/app/test-folder/page.js
+++ src/app/test-folder/page.js
@@ -1,4 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ApiNetworkProvider } from '@multiversx/sdk-network-providers/out';
+
 export default function TestFolder() {
+
+  const [networkStatus, setNetworkStatus] = useState(null);
+
+  useEffect(() => {
+    const provider = new ApiNetworkProvider('https://devnet-gateway.multiversx.com');
+    
+    async function fetchNetworkStatus() {
+      try {
+        const status = await provider.getNetworkStatus();
+        console.log('Network status:', status);  // Logging the network status to the console
+        setNetworkStatus(status);
+      } catch (error) {
+        console.error('Error fetching network status:', error);
+      }
+    }
+
+    fetchNetworkStatus();
+  }, []);
+
+
   return <section className="bg-white dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
       <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">test-folder</h1>
