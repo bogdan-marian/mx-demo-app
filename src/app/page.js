1e848c10a4b30e3163f5eed1b708aa23e2d555b1
bogdan <bogdan.oloeriu@gmail.com>
2024-07-07 09:28:13 +0200

    demo app

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function MxDemoApp() {
 
   return <section className="bg-white dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">mx-dem-app</h1>
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">mx-demo-app</h1>
       <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">This is a simple mx demo app</p>
       <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
         <p>Hello test demo app</p>
